5ea1754a89177945a8236dcb36dff36a2e7f8246
fdomf <fdomenef@gmail.com>
2020-05-18 20:00:24 +0200

    Minor changes

--- ros_ws/src/camera_servcli/camera_servcli/camera_service.py
+++ ros_ws/src/camera_servcli/camera_servcli/camera_service.py
@@ -4,7 +4,6 @@ import rclpy
 from rclpy.node import Node
 
 from .camera import CameraWebService
-import ephem
 
 class CameraService(Node):
 
@@ -12,15 +11,6 @@ class CameraService(Node):
         super().__init__('camera_service')
         self.srv = self.create_service(Camera, 'photo_feed', self.callback)
         self.camera = CameraWebService()
-        obs = ephem.Observer()
-        obs.date = ephem.localtime(ephem.now())
-        obs.lon = '2.2531699035940456'
-        obs.lat = '41.63773122248425'
-        obs.elevation = 207
-        vega = ephem.Sun()
-        vega.compute(obs)
-        print("alt: %s" % (vega.alt))
-        print("az: %s" % (vega.az))
 
     def callback(self, request, response):
         self.camera_action(request, response)
