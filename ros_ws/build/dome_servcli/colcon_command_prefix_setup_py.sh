410f500de8a77737dc97d7616512893435d1d287
fdomf <fdomenef@gmail.com>
2020-05-18 20:38:36 +0200

    Test gitignore

--- ros_ws/build/dome_servcli/colcon_command_prefix_setup_py.sh
+++
@@ -1,2 +0,0 @@
-# generated from colcon_core/shell/template/command_prefix.sh.em
-. "/home/francesc/Escritorio/ROS-observatory/ros_ws/install/observatory_interfaces/share/observatory_interfaces/package.sh"
--- ros_ws/build/observatory_interfaces/observatory_interfaces__py/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/francesc/Escritorio/ROS-observatory/ros_ws/build/observatory_interfaces/observatory_interfaces__py
-# Build directory: /home/francesc/Escritorio/ROS-observatory/ros_ws/build/observatory_interfaces/observatory_interfaces__py
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
